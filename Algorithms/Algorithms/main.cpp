2caed1dc5d2d720ac4d25ba9c8c09241526e2850
Gaurav Yadav <gauravyadav2099@gmail.com>
2016-05-31 22:13:30 +0530

    hello world program

---
+++ Algorithms/Algorithms/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  Algorithms
+//
+//  Created by Yadav, Gaurav on 5/31/16.
+//  Copyright © 2016 Yadav, Gaurav. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    int i =0;
+    for (i=0;i<10;i++){
+        std::cout << i  <<std::endl ;
+    }
+    return 0;
+}
