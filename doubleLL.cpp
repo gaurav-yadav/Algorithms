f02bbd127a76e4ab4e9bece04454d15631d8df9e
Gaurav Yadav <gauravyadav2099@gmail.com>
2016-06-04 00:21:01 +0530

    delete fucntion added to list

--- doubleLL.cpp
+++ doubleLL.cpp
@@ -44,6 +44,40 @@ while(iterator){
 	std::cout << iterator->data << "-->";
 	iterator=iterator->next;
 }
+std::cout << std::endl;
+}
+
+void deletell(int x){
+	node *iterator=head;
+	if(iterator==NULL)
+		return; //nothing to delete empty LL
+	//edge cases in using a single pointer is delting the first and last elt.. 
+	if(iterator->data==x)
+	{
+		head=iterator->next;
+		iterator->next->prev=NULL; //break the backward connection
+		iterator->next=NULL;
+		free(iterator);
+		return; //we are done here lets go back
+	}
+	while(iterator->next->data!=x || iterator==NULL) //either we found or we reached end -- not fixing it now
+	{
+		iterator=iterator->next;
+	}
+	//found the element
+	//if its last elt-- iterator->next->NULL
+	if(iterator->next->next==NULL){
+
+	
+	iterator->next=NULL;
+	free(iterator->next);
+	}
+	else
+	{
+	//iterator--> next(to be deleted)-->next 
+	iterator->next=iterator->next->next;
+	iterator->next->next->prev=iterator;  //ye phtega end element pe without if 
+	}
 
 }
 
@@ -55,6 +89,10 @@ int i=0;
 for (i=0 ;i<10;i++){
 	insert(i);
 }
+printll();
+deletell(9);
+printll();	
+deletell(0);
 printll();
 
 	return 0;
